67344818a82617cf598a57ae86f8d7502c797c08
Andreas Litt <andreas.litt@gmail.com>
2013-12-17 18:05:59 +0100

    Added pyenv support.

--- zsh/theme.zsh
+++ zsh/theme.zsh
@@ -3,7 +3,11 @@ _rbenv_prompt_info() {
 	[[ -a .ruby-version ]] && echo "($(rbenv_prompt_info))"
 }
 
-PROMPT='$fg_bold[blue][$fg[red]%t$fg_bold[blue]]$fg_bold[blue][$fg[red]%n@%m:%~$fg_bold[blue]]$(git_prompt_info)$fg[yellow]$(_rbenv_prompt_info)$reset_color
+_pyenv_prompt_info() {
+	[[ -a .python-version ]] && head -n 1 .python-version
+}
+
+PROMPT='$fg_bold[blue][$fg[red]%t$fg_bold[blue]]$fg_bold[blue][$fg[red]%n@%m:%~$fg_bold[blue]]$(git_prompt_info)$fg[yellow]$(_rbenv_prompt_info) $(_pyenv_prompt_info)$reset_color
  $ '
 # git theming
 ZSH_THEME_GIT_PROMPT_PREFIX="$fg_bold[green]("
