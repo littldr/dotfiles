9e6ba508454045aaebbbaa51d759fef7815bb89c
Andreas Litt <andreas.litt@gmail.com>
2013-12-28 16:52:58 +0100

    improved pubkey function to support multiple rsa pub keys.

--- zsh/system/keys.zsh
+++
@@ -1,3 +0,0 @@
-# Pipe my public key to my clipboard.
-# Source https://raw.github.com/pgolm/dotfiles/master/zsh/system/keys.zsh
-alias pubkey="more ~/.ssh/id_rsa.pub | pbcopy | echo '=> Public key copied to pasteboard.'"
