ea8b3ef20dcf5b6c038395e99173c55a81cdaae9
Andreas Litt <andreas.litt@gmail.com>
2015-03-09 15:02:30 +0100

    add GO Path

--- zsh/path.zsh
+++ zsh/path.zsh
@@ -1,3 +1,5 @@
 export PATH="$HOME/.bin:$HOME/.rbenv/shims:/usr/local/bin:/usr/local/sbin:$PATH"
-
 export ANDROID_HOME="/usr/local/opt/android-sdk"
+
+export GOPATH="$HOME/Workspace/GO"
+export PATH=$PATH:$GOPATH/bin
