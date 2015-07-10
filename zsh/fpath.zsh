5d0a4525165e65d458c6e47d0a7cb39aadf3ab68
Andreas Litt <andreas.litt@wlw.de>
2015-07-10 17:04:18 +0200

    add all installed completion files

--- zsh/fpath.zsh
+++ zsh/fpath.zsh
@@ -1,2 +1,6 @@
 #add each topic folder to fpath so that they can add functions and completion scripts
 for topic_folder ($ZSH/*) if [ -d $topic_folder ]; then  fpath=($topic_folder $fpath); fi;
+
+if [[ -d /usr/local/share/zsh/site-functions ]]; then
+  fpath=(/usr/local/share/zsh/site-functions $fpath)
+fi
