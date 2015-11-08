05ce8e4d4df4fa6d8b5fea55e12fc9c0b7030832
Andreas Litt <andreas.litt@gmail.com>
2015-11-08 14:56:54 +0100

    :boom: improve fish prompt

--- tag-fish/config/fish/functions/fish_prompt.fish
+++ tag-fish/config/fish/functions/fish_prompt.fish
@@ -1,23 +1,44 @@
-function fish_prompt --description 'Write out the prompt'
-	# Just calculate this once, to save a few cycles when displaying the prompt
-	if not set -q __fish_prompt_hostname
-		set -g __fish_prompt_hostname (hostname|cut -d . -f 1)
-	end
+# Source: https://gist.github.com/KELiON/4721946
+function _git_branch_name
+  echo (git symbolic-ref HEAD ^/dev/null | sed -e 's|^refs/heads/||')
+end
 
-	set -l color_cwd
-	set -l suffix
-	switch $USER
-	case root toor
-		if set -q fish_color_cwd_root
-			set color_cwd $fish_color_cwd_root
-		else
-			set color_cwd $fish_color_cwd
-		end
-		set suffix '#'
-	case '*'
-		set color_cwd $fish_color_cwd
-		set suffix '>'
-	end
+function _is_git_dirty
+  echo (git status -s --ignore-submodules=dirty ^/dev/null)
+end
 
-	echo -n -s "$USER" @ "$__fish_prompt_hostname" ' ' (set_color $color_cwd) (prompt_pwd) (set_color normal) "$suffix "
+function _rb_prompt
+  echo (rbenv version | awk '{print ($1 != "system") ? $1 : ""}')
+end
+
+function fish_prompt
+  set -l yellow (set_color -o yellow)
+  set -l red (set_color -o red)
+  set -l blue (set_color -o blue)
+  set -l green (set_color -o green)
+  set -l normal (set_color normal)
+
+  set -l arrow "$red➜ $normal"
+  set -l cwd $green(prompt_pwd)
+
+  set -l sep "$blue | $normal"
+
+  set -l ruby_version (_rb_prompt)
+  if [ -n "$ruby_version" ]
+    set ruby_version "$sep$red$ruby_version$normal"
+  end
+
+  if [ (_git_branch_name) ]
+    set -l git_branch $red(_git_branch_name)
+    set git_info "$sep$git_branch"
+    if [ (_is_git_dirty) ]
+      set -l dirty "$yellow✗$normal"
+      set git_info "$git_info$dirty"
+    else
+      set -l clean "$green✔︎$normal"
+      set git_info "$git_info$clean"
+    end
+  end
+
+  echo -s $cwd $git_info $ruby_version \n $arrow
 end
