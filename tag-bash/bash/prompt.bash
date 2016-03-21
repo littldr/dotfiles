074dac0ff5402b1a8d875c4297cdbf131a749a55
Andreas Litt <andreas.litt@gmail.com>
2016-03-21 20:47:22 +0100

    :art: made my prompt awesome again

--- tag-bash/bash/prompt.bash
+++ tag-bash/bash/prompt.bash
@@ -1,1 +1,30 @@
-echo ">"
+GREEN="\033[1;32m"
+BLUE="\033[0;34m"
+RED="\033[0;31m"
+MAGENTA="\033[0;35m"
+NO_COLOR="\033[0m"
+
+GIT_PROMPT_CLEAN='✓'
+GIT_PROMPT_DIRTY='✗'
+
+_git_prompt_info() {
+  ref=$(git symbolic-ref HEAD 2> /dev/null) || ref=$(git rev-parse --short HEAD 2> /dev/null) || return
+  ref=${ref#refs/heads/}
+  if [[ ! -z ref ]]; then
+    GIT_STATUS="$(git status -s 2> /dev/null | tail -n1)"
+    echo -en " $MAGENTA<$ref"
+    if [[ -n $GIT_STATUS ]]; then
+      echo -en "$RED$GIT_PROMPT_DIRTY"
+    else
+      echo -en "$GREEN$GIT_PROMPT_CLEAN"
+    fi
+    echo -en "$MAGENTA>$NO_COLOR"
+  fi
+}
+
+_rbenv_prompt_info() {
+	[[ -e .ruby-version ]] && echo -en " $RED<$(rbenv version-name)>$NO_COLOR"
+}
+
+export PS1="╭─\$(tput bold)$GREEN\u@\h $BLUE\w\$(_git_prompt_info)\$(_rbenv_prompt_info)\$(tput sgr0)
+╰─𝇇 "
