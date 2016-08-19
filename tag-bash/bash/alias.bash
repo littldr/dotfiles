612528381716916a1044ef0f38e28903e3eb47c1
Andreas Litt <andreas.litt@gmail.com>
2016-08-19 14:36:16 +0200

    Customize man page colors

--- tag-bash/bash/alias.bash
+++ tag-bash/bash/alias.bash
@@ -9,3 +9,15 @@ else
 fi
 
 alias doc='docker-compose'
+
+man() {
+  # Source: http://boredzo.org/blog/archives/2016-08-15/colorized-man-pages-understood-and-customized
+  env \
+    LESS_TERMCAP_md=$(printf "\e[1;34m") \
+    LESS_TERMCAP_me=$(printf "\e[0m") \
+    LESS_TERMCAP_so=$(printf "\e[1;36;31m") \
+    LESS_TERMCAP_se=$(printf "\e[0m") \
+    LESS_TERMCAP_us=$(printf "\e[1;32m") \
+    LESS_TERMCAP_ue=$(printf "\e[0m") \
+      man "$@"
+}
