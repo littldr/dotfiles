f9fa2080b050906eca485dd34f9eeed87e0c1621
Andreas Litt <andreas.litt@gmail.com>
2026-02-25 19:03:16 +0100

    feat: update dotfiles configuration

--- tag-bash/bash/path.bash
+++ tag-bash/bash/path.bash
@@ -17,7 +17,7 @@ if [ -d ${SYSTEM_PREFIX}/opt/gnu-sed/libexec/gnubin ]; then
 fi
 
 if [ -d ${SYSTEM_PREFIX}/opt/curl/bin ]; then
-  PATH="${SYSTEM_PREFIX}/opt/curl/bin:$PATH"
+  PATH="${SYSTEM_PREFIX}/opt/curl/bin:${PATH}"
   MANPATH="${SYSTEM_PREFIX}/opt/curl/share/man:${MANPATH}"
 fi
 
@@ -25,3 +25,9 @@ PATH="${PATH}:./node_modules/.bin"
 PATH="${PATH}:${HOME}/.bin"
 PATH="${PATH}:${HOME}/.krew/bin"
 PATH="${PATH}:${HOME}/go/bin/"
+PATH="${PATH}:${HOME}/.local/bin"
+
+if [ -f ${SYSTEM_PREFIX}/opt/asdf/bin/asdf ]; then
+  # asdf is installed. Prepanding shims to PATH, in order to override system binaries
+  export PATH="${HOME}/.asdf/shims:${PATH}"
+fi
